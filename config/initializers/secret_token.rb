ba8a2fad2802b007f23b69daeb38c47f5f002e4c
oheric <ohericyoung@gmail.com>
2015-02-13 15:49:01 -0800

    Initialize Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lovebuzz::Application.config.secret_key_base = 'e9ebe495e1ee44d810e945bde7aad812e617751937ea17bfc3188014959b27f2abfeb94835b53323f08eca9880d272e64fdda00a4e7db2a7ce7bbe48271008be'
