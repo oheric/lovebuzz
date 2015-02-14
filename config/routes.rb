de0521e52f9ce9cf41817370111ace81e15d00ce
oheric <ohericyoung@gmail.com>
2015-02-13 23:15:30 -0800

    fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Lovebuzz::Application.routes.draw do
-  root 'welcome#index'
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
