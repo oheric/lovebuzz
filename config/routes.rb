d0b9ac7ec41eb054601d41b8ae7af654acd19247
oheric <ohericyoung@gmail.com>
2015-02-18 02:16:35 -0800

    styled landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Lovebuzz::Application.routes.draw do
   root 'home#index'
+  get 'signup' => 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
