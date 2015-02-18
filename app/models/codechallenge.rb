efd0852bbd9a0473dfff5575ccbd255145004053
oheric <ohericyoung@gmail.com>
2015-02-18 15:54:04 -0800

    user validations added

--- app/models/codechallenge.rb
+++
@@ -1,3 +0,0 @@
-class Person < ActiveRecord::Base
-  attr_accessible :grandchildren
-has_many
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Lovebuzz::Application.routes.draw do
   root 'home#index'
+  get 'about' => 'home#about'
   get 'signup' => 'users#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
