c6c528530cefb43c3fc60786b34e12702de33a01
oheric <ohericyoung@gmail.com>
2015-02-21 00:17:17 -0800

    added logout with forgetting users

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,13 @@ class User < ActiveRecord::Base
   has_many :quizzes
   has_secure_password
 
+  # Returns the hash digest of the given string
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+    BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
   # Returns a random token
   def User.new_token
     SecureRandom.urlsafe_base64
@@ -20,8 +27,13 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  Returns true if the given token matches the digest. 
+  # Returns true if the given token matches the digest. 
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a User
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
