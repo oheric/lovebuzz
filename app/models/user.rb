f09a956e6701eb0ff845e79140e23410d9ed94f7
oheric <ohericyoung@gmail.com>
2015-02-22 19:32:12 -0800

    created activation token and digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates_uniqueness_of :email
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates_length_of :email, maximum: 255
@@ -36,5 +37,18 @@ class User < ActiveRecord::Base
   # Forgets a User
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  # Converts email to all lower-case
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  # Creates and assigns the activation token and digest
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
