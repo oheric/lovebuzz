69b4ab30f0520892ad6b69b7ea4243df409354bd
oheric <ohericyoung@gmail.com>
2015-02-23 20:34:21 -0800

    added password resets and email configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -63,16 +63,20 @@ class User < ActiveRecord::Base
     UserMailer.password_reset(self).deliver_now
   end
 
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
+
   private
 
-  # Converts email to all lower-case
-  def downcase_email
-    self.email = email.downcase
-  end
+    # Converts email to all lower-case
+    def downcase_email
+      self.email = email.downcase
+    end
 
-  # Creates and assigns the activation token and digest
-  def create_activation_digest
-    self.activation_token = User.new_token
-    self.activation_digest = User.digest(activation_token)
+    # Creates and assigns the activation token and digest
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
   end
-end
