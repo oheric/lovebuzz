17bd2c9c5d2b2fa53b7243876b5d6a5b94a11e46
oheric <ohericyoung@gmail.com>
2015-02-24 00:57:51 -0800

    activation and password reset emails working

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,11 @@ class UserMailer < ActionMailer::Base
 
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: "Account activation", from: "'NoReply' <noreply@lovebuzzapp.herokuapp.com>"
   end
 
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: "Password reset"
+    mail to: user.email, subject: "Password reset", from: "'NoReply' <noreply@lovebuzzapp.herokuapp.com>"
   end
 end
