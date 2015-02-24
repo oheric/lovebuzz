3c6e7fcdfef260eda3d57eaaf6bd2f3336f0714b
oheric <ohericyoung@gmail.com>
2015-02-24 02:01:39 -0800

    fixed settings, temporary activation

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,11 +2,11 @@ class UserMailer < ActionMailer::Base
 
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation", from: "'NoReply' <noreply@lovebuzzapp.herokuapp.com>"
+    mail to: user.email, subject: "Activate Your QNDATE Account", from: "'QNDATE' <customerservice@lovebuzzapp.herokuapp.com>"
   end
 
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: "Password reset", from: "'NoReply' <noreply@lovebuzzapp.herokuapp.com>"
+    mail to: user.email, subject: "Password Reset", from: "'QNDATE' <customerservice@lovebuzzapp.herokuapp.com>"
   end
 end
