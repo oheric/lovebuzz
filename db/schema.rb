1049bfe752e1c469173a134084607a543017547e
oheric <ohericyoung@gmail.com>
2015-03-01 21:04:21 -0800

    added results model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150301101915) do
+ActiveRecord::Schema.define(version: 20150302050347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,11 @@ ActiveRecord::Schema.define(version: 20150301101915) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "results", force: true do |t|
+    t.integer "result_value"
+    t.string  "content"
   end
 
   create_table "users", force: true do |t|
